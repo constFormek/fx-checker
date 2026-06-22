47a46ca7a27e473af8e987734f361d590950c486
plexi <plexify.code@gmail.com>
2026-06-22 14:31:03 +0200

    changed interface name, passed 1 as a hardcoded amount to the fetch call

--- lib/fetchExchangeRate.ts
+++ lib/fetchExchangeRate.ts
@@ -1,10 +1,9 @@
 interface fetchExchangeRateProps {
   base: string; // type of the possible currencies
   symbol: string;
-  amount: number;
 }
 
-export type response = {
+export type exchangeObject = {
   amount: number;
   base: string;
   date: string;
@@ -15,13 +14,11 @@ export type response = {
 export const fetchExchangeRate = async ({
   base,
   symbol,
-  amount,
-}: fetchExchangeRateProps): Promise<response> => {
+}: fetchExchangeRateProps): Promise<exchangeObject> => {
   try {
     const data = await fetch(
-      `https://api.frankfurter.dev/v1/latest?amount=${amount}&base=${base}&symbols=${symbol}`,
+      `https://api.frankfurter.dev/v1/latest?amount=${1}&base=${base}&symbols=${symbol}`,
     );
-
 
     if (!data.ok) {
       throw new Error("idk but something went wrong probably with them");
