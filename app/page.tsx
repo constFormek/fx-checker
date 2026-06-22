c425ed67b316c011ed9d0c7e90a6f943bef111b6
plexi <plexify.code@gmail.com>
2026-06-22 14:28:45 +0200

    passed initial data to the converter

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,22 @@
 import Converter from "@/components/Converter";
+import { fetchExchangeRate } from "@/lib/fetchExchangeRate";
 
 export default async function Home() {
+  const base = "USD";
+  const symbol = "EUR";
+  const initialExchangeData = await fetchExchangeRate({
+    base: base,
+    symbol: symbol,
+  });
   return (
     <div className="flex flex-col bg-neutral-900 font-jetbrains-mono w-screen h-screen overflow-hidden items-center justify-center">
       <h1 className="font-jetbrains-mono">CHECK THE RATE</h1>
 
-      <Converter />
+      <Converter
+        base={base}
+        symbol={symbol}
+        initialExchangeRate={initialExchangeData.rates[symbol]}
+      />
     </div>
   );
 }
