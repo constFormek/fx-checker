8692841977ad30b33f73b9933aac35d0d9fef786
plexi <plexify.code@gmail.com>
2026-06-23 16:41:24 +0200

    function that creates an object for every currency, updated properties from exchange rate function

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,49 @@
 import Converter from "@/components/Converter";
+import CurrencyHydrator from "@/components/CurrencyHydrator";
 import { fetchCurrenciesData, fetchExchangeRate } from "@/lib/currencyApi";
 
+import { readdirSync } from "fs";
+import path from "path";
+
 export default async function Home() {
-  const base = "USD";
-  const symbol = "EUR";
-  const initialExchangeData = await fetchExchangeRate({
-    base: base,
-    symbol: symbol,
-  });
+  const flagFiles = readdirSync(
+    path.join(process.cwd(), "public/images/flags"),
+  );
+  const supportedCountryCodes = flagFiles.map((f) => f.replace(".webp", ""));
 
   const currenciesData = await fetchCurrenciesData();
 
-  return (
-    <div className="flex flex-col bg-neutral-900 font-jetbrains-mono w-screen h-screen overflow-hidden items-center justify-center">
-      <h1 className="font-jetbrains-mono">CHECK THE RATE</h1>
+  const supportedCurrencies = currenciesData
+    .filter((c) =>
+      supportedCountryCodes.includes(
+        c.iso_code.slice(0, 2).toLocaleLowerCase(),
+      ),
+    )
+    .map((c) => {
+      return {
+        iso_code: c.iso_code,
+        name: c.name,
+        flag: `/images/flags/${c.iso_code.slice(0, 2).toLocaleLowerCase()}.webp`,
+      };
+    });
 
-      <Converter
-        base={base}
-        symbol={symbol}
-        initialExchangeRate={initialExchangeData.rates[symbol]}
-      />
-    </div>
+  const initialExchangeData = await fetchExchangeRate({
+    base: "USD",
+    quote: "EUR",
+  });
+  return (
+    <>
+      <div className="flex flex-col bg-neutral-900 font-jetbrains-mono w-screen h-screen overflow-hidden items-center ">
+        <h1 className="font-jetbrains-mono">CHECK THE RATE</h1>
+
+        <Converter
+          base={"USD"}
+          quote={"EUR"}
+          initialExchangeRate={initialExchangeData.rate}
+        />
+      </div>
+
+      <CurrencyHydrator currencies={supportedCurrencies} />
+    </>
   );
 }
