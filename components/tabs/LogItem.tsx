f05797d6c6029c90b25f05ded62d1e7c8ef54c75
plexi <plexify.code@gmail.com>
2026-07-08 22:32:30 +0200

    feat empty state, basic ui for the header

---
+++ components/tabs/LogItem.tsx
@@ -0,0 +1,54 @@
+import { Pair, useCurrencies } from "@/lib/currenciesStore";
+import Icon from "../Icon";
+import { formatTime } from "@/lib/helpers";
+
+interface logItemProps {
+  id: string;
+  pair: Pair;
+  timestamp: number;
+  receiveAmount: string;
+  sendAmount: string;
+}
+
+const LogItem = ({
+  pair,
+  timestamp,
+  receiveAmount,
+  sendAmount,
+  id,
+}: logItemProps) => {
+  const timeElapsed = formatTime(timestamp);
+  const deleteLog = useCurrencies((s) => s.deleteLog);
+  return (
+    <div className="rounded-10 flex w-full items-center justify-between border border-neutral-500 bg-neutral-600 px-4 py-3">
+      <div className="flex flex-col gap-2 md:flex-row">
+        <p className="max-w-16 text-neutral-200">{timeElapsed}</p>
+
+        <div className="text-preset-4 flex flex-row items-center gap-2">
+          {pair.base}
+          <Icon name="arrow-right" size={12} className="text-neutral-200" />
+          {pair.quote}
+        </div>
+      </div>
+
+      <div className="flex items-center gap-2.5">
+        <div className="text-preset-3 flex flex-col items-end gap-2 md:flex-row">
+          <p className="text-neutral-50">{sendAmount}</p>
+
+          <p className="text-lime-500">{receiveAmount}</p>
+        </div>
+
+        <button
+          onClick={() => {
+            deleteLog(id);
+          }}
+          className="rounded-8 border border-neutral-500 bg-neutral-600 p-2 text-neutral-50"
+        >
+          <Icon name="delete" size={16} />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default LogItem;
