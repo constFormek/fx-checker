b41c9fc49f8f3bf7e51a4eb5fc3f9be7ebf84e06
plexi <plexify.code@gmail.com>
2026-07-09 14:58:22 +0200

    feat: customizable styles

--- components/ChangeIndicator.tsx
+++ components/ChangeIndicator.tsx
@@ -1,33 +1,37 @@
 import Icon from "./Icon";
 
-const stylesMap = {
+export const stylesMap = {
   up: {
     color: "text-green-500",
     prefix: "+",
-    Icon: <Icon name="chevron-down" size={12} className="rotate-180" />,
+    Icon: <Icon name="chevron-down" className="h-[1em] w-[1em] rotate-180" />,
   },
   down: {
     color: "text-red-500",
     prefix: "",
-    Icon: <Icon name="chevron-down" size={12} className="rotate-0" />,
+    Icon: <Icon name="chevron-down" className="h-[1em] w-[1em] rotate-0" />,
   },
   flat: { color: "text-neutral-200", prefix: "", Icon: <></> },
 };
 
-const getDirection = (change: number) => {
+export const getDirection = (change: number) => {
   return change > 0 ? "up" : change < 0 ? "down" : "flat";
 };
 
 interface changeIndicatorProps {
   changePercentage: number;
+  className?: string;
 }
 
-const ChangeIndicator = ({ changePercentage }: changeIndicatorProps) => {
+const ChangeIndicator = ({
+  changePercentage,
+  className,
+}: changeIndicatorProps) => {
   const direction = getDirection(changePercentage);
 
   return (
     <div
-      className={`${stylesMap[direction].color} text-preset-6 flex items-center`}
+      className={`${stylesMap[direction].color} ${className ?? "text-preset-6"} flex items-center`}
     >
       {stylesMap[direction].Icon}
 
