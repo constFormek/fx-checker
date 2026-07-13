1b4d6630531ba800226d5e7e061f853a2c3bedeb
plexi <plexify.code@gmail.com>
2026-07-13 12:21:45 +0200

    fix: removed aria-hidden from the ticker

--- components/Ticker.tsx
+++ components/Ticker.tsx
@@ -61,10 +61,7 @@ const Ticker = () => {
 
 export const TickerSkeleton = () => {
   return (
-    <div
-      aria-hidden
-      className="text-preset-6 md:text-preset-5-medium flex items-center"
-    >
+    <div className="text-preset-6 md:text-preset-5-medium flex items-center">
       <div className="flex items-center gap-2 bg-lime-500 px-2 py-3 text-neutral-900 uppercase md:px-4 md:py-3">
         <div className="aspect-square w-1.5 rounded-full bg-neutral-900"></div>
 
